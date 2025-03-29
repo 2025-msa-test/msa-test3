06b2b29efe79425444783e9a6e9dcc1a6bd995e0
choieuntaek <choiet0225@gmail.com>
2025-03-29 18:31:21 +0900

    cicd test6

--- src/main/java/com/example/msatest3/customer/controller/CustomerController.java
+++ src/main/java/com/example/msatest3/customer/controller/CustomerController.java
@@ -15,6 +15,6 @@ public class CustomerController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
         customerService.deleteCustomer(id);
-        return new ResponseEntity<>("Customer deleted successfully - cicd test5", HttpStatus.OK);
+        return new ResponseEntity<>("Customer deleted successfully - cicd test6", HttpStatus.OK);
     }
 }
